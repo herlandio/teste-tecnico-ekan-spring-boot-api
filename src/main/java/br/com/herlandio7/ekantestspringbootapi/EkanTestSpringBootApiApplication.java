cdd2bac7b2e2eb690a7266d5d6d58209065d00f3
herlandio <herlandio7@gmail.com>
2024-03-11 16:21:43 -0300

    start development

---
+++ src/main/java/br/com/herlandio7/ekantestspringbootapi/EkanTestSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7.ekantestspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkanTestSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkanTestSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/herlandio7/ekantestspringbootapi/EkanTestSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7.ekantestspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkanTestSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
