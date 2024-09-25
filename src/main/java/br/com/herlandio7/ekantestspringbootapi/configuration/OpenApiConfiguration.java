481bfdc53969d43d5c2af6c9893b55dd0bfb73ce
Herlandio <herlandio7@gmail.com>
2024-09-25 17:54:19 -0300

    add basic auth swagger

--- src/main/java/br/com/herlandio7/ekantestspringbootapi/configuration/OpenApiConfiguration.java
+++ src/main/java/br/com/herlandio7/ekantestspringbootapi/configuration/OpenApiConfiguration.java
@@ -1,26 +1,33 @@
 package br.com.herlandio7.ekantestspringbootapi.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Beneficiary Api", version = "v1", description = "Documentation beneficiaries api"))
 public class OpenApiConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
                 .info(
-                        new io.swagger.v3.oas.models.info.Info()
-                                .title("Beneficiary Api")
-                                .version("v1")
-                                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-
+                    new Info()
+                    .title("Beneficiary Api")
+                    .version("1.0")
+                    .license(new License().name("License Apache 2.0").url("http://springdoc.org"))
+                    .description("Manager beneficiaries and documents"))
+                .addSecurityItem(
+                    new SecurityRequirement()
+                    .addList("basicAuth"))
+                .components(
+                    new Components()
+                    .addSecuritySchemes("basicAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("basic")));
     }
 }
